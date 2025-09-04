788dc9bf85a6f47bebceee154dbcc12495861c1e
tristan_crawford <164209993+OhwaitisthatTris@users.noreply.github.com>
2025-09-04 12:35:21 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,8 @@
+"""
+
+I looked through phython.org and found information about the latest version of Python and its features. 
+If I had great programming skills, i would first write a script to scrape the website for updated versions of phython.
+After it retrieves the latest version, it would then compare it to the currently installed version on my machine and
+ notify me if an update is available. This would keep me focused on making a new project without worrying about bugs
+and failed installations due to outdated software.
+"""
---
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,9 @@
+'''
+
+If I had unlimited power and knowledge in programming, i would try to make my own world.
+I would create a virtual enviroment where I could simulate real life things that can/could happen in the real world.
+It would be limited only by imagination and creativity. I would control the weather, the seasons, even how the 
+ocean waves crash against the shore. I would create a world where people can create their own stories and adventures,
+and where they can learn and grow from their experiences.
+
+'''
